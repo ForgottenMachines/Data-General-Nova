520dd20757b0c2a60c223e1ad1490d588d9b2738
Forgotten Machines <forgottenmachines@gmail.com>
2025-03-14 08:04:48 -0500

    arduino

--- DG Nova CPU Tester/Piggyback MEM0-7/Arduino_Addressing/Arduino_Addressing.ino
+++ DG Nova CPU Tester/Piggyback MEM0-7/Arduino_Addressing/Arduino_Addressing.ino
@@ -4,6 +4,19 @@ String EnterData = "126440";
 void setup() {
   Serial.begin(115200);
 
+//for c = 1 to len(MyString)
+//Hex = Mid(MyString,len(MyString),1)
+//Next c
+
+#include <string>
+
+std::string MyString = "YourStringHere"; // Replace with your actual string
+std::string Hex;
+
+for (size_t c = 1; c <= MyString.length(); ++c) {
+    Hex = MyString[MyString.length() - 1]; // Get the last character
+};
+
 
 
 }
