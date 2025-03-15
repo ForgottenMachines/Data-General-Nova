35163980c7fa16a90992f53b1f4fe04507e2a773
Forgotten Machines <forgottenmachines@gmail.com>
2025-03-14 20:57:55 -0500

    i

--- DG Nova CPU Tester/Piggyback MEM0-7/Arduino_Addressing/Arduino_Addressing.ino
+++ DG Nova CPU Tester/Piggyback MEM0-7/Arduino_Addressing/Arduino_Addressing.ino
@@ -1,28 +1,29 @@
 String Address = "07757";
 String EnterData = "126440";
+String Octal = "0";
+String Binary = "";
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("====================");
 
 //for c = 1 to len(MyString)
 //Hex = Mid(MyString,len(MyString),1)
+//Hex = Mid(Address,c,1)
 //Next c
 
-#include <string>
-
-std::string MyString = "YourStringHere"; // Replace with your actual string
-std::string Hex;
-
-for (size_t c = 1; c <= MyString.length(); ++c) {
-    Hex = MyString[MyString.length() - 1]; // Get the last character
+for (size_t c = 1; c <= Address.length(); ++c) {
+  Octal = Address[c - 1]; // Get the last character
+  Serial.println(Octal);
+if(Octal == "0") {Binary = Binary + "000";};
+if(Octal == "5") {Binary = Binary + "101";};
+if(Octal == "7") {Binary = Binary + "111";};
 };
-
-
 
 }
 
 void loop() {
-  Serial.println("Hi");
-  delay(200);
+ Serial.println(Binary);
+   delay(20000);
 
 }
