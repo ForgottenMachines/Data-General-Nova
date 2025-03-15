ec40e3ca80073918b97e54cc8e7593b6b5f2e4da
Forgotten Machines <forgottenmachines@gmail.com>
2025-03-14 23:33:32 -0500

    Update Arduino_Addressing.ino

--- DG Nova CPU Tester/Piggyback MEM0-7/Arduino_Addressing/Arduino_Addressing.ino
+++ DG Nova CPU Tester/Piggyback MEM0-7/Arduino_Addressing/Arduino_Addressing.ino
@@ -4,6 +4,7 @@ String Octal = "0";
 String Binary = "0";
 String TestStr = "";
 String Upper = "";
+String Lower = "";
 
 void setup() {
   Serial.begin(115200);
@@ -13,20 +14,26 @@ for (size_t c = 1; c <= Address.length(); ++c) {
   Octal = Address[c - 1]; // Get the last character
   Serial.println(Octal);
 if(Octal == "0") {Binary = Binary + "000";};
+if(Octal == "1") {Binary = Binary + "001";};
+if(Octal == "2") {Binary = Binary + "010";};
+if(Octal == "3") {Binary = Binary + "011";};
+if(Octal == "4") {Binary = Binary + "100";};
 if(Octal == "5") {Binary = Binary + "101";};
+if(Octal == "6") {Binary = Binary + "110";};
 if(Octal == "7") {Binary = Binary + "111";};
 };
 
-Binary = "1234567890123456";
+//Binary = "1234567890123456";
 
-Upper = Binary.SubStr(7, 8);
-  Serial.println(Upper);
+Upper = Binary.substring(0, 8);
+Lower = Binary.substring(8, 16);
 
 }
 
 void loop() {
  Serial.println(Binary);
  Serial.println(Upper);
-    delay(20000);
+ Serial.println(Lower);
+     delay(20000);
 
 }
